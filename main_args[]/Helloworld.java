52b0b2f1889c3c1149371f9e7a99f48401472f91
LiSTlst <2510737554@qq.com>
2016-05-15 03:51:37 +0800

    main参数部分

---
+++ main_args[]/Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld{
+	public static void main(String args[]){
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+	}
+}
